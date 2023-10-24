04196197062f61660bf3d5e3d7f71da74393434a
ronil74 <ronilrodrigues74@gmail.com>
2023-10-24 18:44:20 +0100

    Test openshift

--- src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/ContactRestService.java
+++ src/main/java/uk/ac/newcastle/enterprisemiddleware/contact/ContactRestService.java
@@ -127,7 +127,7 @@ public class ContactRestService {
     @Cache
     @Path("/{id:[0-9]+}")
     @Operation(
-            summary = "Fetch a Contact by id",
+            summary = "Fetch a Contact by id (test)",
             description = "Returns a JSON representation of the Contact object with the provided id."
     )
     @APIResponses(value = {
